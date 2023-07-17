cbed377f908e2c1ea54114ea90bdc9abda0e005e
SeongEun <bongfam2@naver.com>
2023-07-17 18:07:53 +0900

    Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="menu">
+    <a v-for="naming in menu" :key="naming">{{ naming }}</a>
+  </div>
+
+  <div class="main"
+    v-for="name,i in products" :key="i">
+    <button class="modal" @click="ifopen = true">
+      {{ name }}
+    </button>
+
+    <p>**만원</p>
+    <button @click="report[i]++">허위매물신고</button>
+    <span>신고수: {{report[i]}}</span>
+    <button @click="ifopen = false">닫기</button>
+  </div>
+
+  <div class="black-bg" v-if="ifopen == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지내용</p>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import oneroom from './data/oneroom.js'
+  export default{
+    name: 'App', 
+    data(){
+      return{
+        menu : ['Home', 'Shop', 'About'],
+        products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+        prices : ['50', '60', '70'],
+        report : [0,0,0],
+        ifopen : false,
+        data: oneroom,
+      }
+    }
   }
-}
 </script>
 
 <style>
@@ -21,6 +49,39 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color : white;
+  padding: 10px;
+}
+body{
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg{
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.modal{
+  background: transparent;
+  border: transparent;
+  font-size: 18px;
+  font-weight: bold;
+}
+.main{
+  margin-top: 5px;
 }
 </style>
