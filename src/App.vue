3f41032d152b403f15f3918c13e2a928e0448dc9
SeongEun <bongfam2@naver.com>
2023-07-25 03:19:49 +0900

    Main Page Using V-for

--- src/App.vue
+++ src/App.vue
@@ -4,84 +4,24 @@
     <a v-for="naming in menu" :key="naming">{{ naming }}</a>
   </div>
 
-  <!-- <div class="black-bg" v-if="ifopen == true">
-    <div class="white-bg">
-      <img src="">
-      <h4>{{room[0].title}}</h4>
-      <p>{{room[0].price}}</p>
-      <button @click="ifopen = false">닫기</button>
-    </div>
-  </div> -->
-  
   <ModalOpen v-bind:state="ifopen"/>
 
-  <div class="main">
+  <div class="main"
+      v-for="(item, index) in room" :key="index">
     <button class="modal" @click="ifopen = true">
-      신림역 원룸
+      {{ item.title }}
     </button>
-    <img src="./assets/room0.jpg"/>
-    <p>{{room[0].price}}</p>
-    <button @click="report[0]++">허위매물신고</button><br>
-    <span>신고수: {{report[0]}}</span>
+    <img :src="require(`@/assets/room${ index }.jpg`)"/>
+    <p>{{ item.price }}</p>
+    <button @click="report[index]++">허위매물신고</button><br>
+    <span>신고수: {{report[index]}}</span>
   </div>
-
-  <div class="main">
-    <button class="modal" @click="ifopen = true">
-      창동 원룸
-    </button>
-    <img src="./assets/room1.jpg"/>
-    <p>{{room[1].price}}</p>
-    <button @click="report[1]++">허위매물신고</button><br>
-    <span>신고수: {{report[1]}}</span>
-  </div>
-
-  <div class="main">
-    <button class="modal" @click="ifopen = true">
-      금산 아파트
-    </button>
-    <img src="./assets/room2.jpg"/>
-    <p>{{room[2].price}}</p>
-    <button @click="report[2]++">허위매물신고</button><br>
-    <span>신고수: {{report[2]}}</span>
-  </div>
-
-  <div class="main">
-    <button class="modal" @click="ifopen = true">
-      더블베드 아파트
-    </button>
-    <img src="./assets/room3.jpg"/>
-    <p>{{room[3].price}}</p>
-    <button @click="report[3]++">허위매물신고</button><br>
-    <span>신고수: {{report[3]}}</span>
-  </div>
-
-  <div class="main">
-    <button class="modal" @click="ifopen = true">
-      명글 아파트
-    </button>
-    <img src="./assets/room4.jpg"/>
-    <p>{{room[4].price}}</p>
-    <button @click="report[4]++">허위매물신고</button><br>
-    <span>신고수: {{report[4]}}</span>
-  </div>
-
-  <div class="main">
-    <button class="modal" @click="ifopen = true">
-      반지하 원룸
-    </button>
-    <img src="./assets/room5.jpg"/>
-    <p>{{room[5].price}}</p>
-    <button @click="report[5]++">허위매물신고</button><br>
-    <span>신고수: {{report[5]}}</span>
-  </div>
-
-
 
 </template>
 
 <script>
   import oneroom from './data/oneroom.js';
-  import ModalOpen from './components/ModalOpen.vue'
+  import ModalOpen from './components/ModalOpen.vue';
   oneroom;
   export default{
     name: 'App', 
